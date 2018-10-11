cc5e7301814ca25a63ec3d90c8d5744aa6702498
cdeath <github@nunopereira.net>
2018-10-11 23:18:03 +0100

    search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,62 @@
-(function() {
-  $('[data-target="#search-modal"]').click(function() {
-    console.log(1);
-    $('#search-modal-input').focus();
-  });
-  $('#search-modal-input').on('input', function(evt) {
-    var results = window.search.filter(function(page) {
-      return page.content.toLowerCase().indexOf(evt.target.value.toLowerCase()) > -1;
+(function($, w, d) {
+
+  var templates = {};
+
+  function getDiacritics(str) {
+    var diacritics = {a: '\u00E0-\u00E3', e: '\u00E9-\u00EA', o: '\u00F3-\u00F5', u: '\u00FA', c: '\u00E7'};
+    str = str.toLowerCase();
+    for (var i = 0, l = str.length, r = '', c; i < l; i++) (c = str.charAt(i), r += diacritics[c] ? '[' + c + diacritics[c] + ']' : c);
+    return r;
+  }
+
+  function search(needle, haystack, options) {
+    options = $.extend(options, {highlight: true, start: 50, stop: 200});
+    needle = needle.replace(/[.?*+^$[\](){}|\\]/g, '');
+    var regex = new RegExp('(' + getDiacritics(needle) + ')', 'gi');
+    if (!regex.test(haystack)) return false;
+    haystack = haystack.replace(regex, '<mark>$1</mark>');
+    var length = haystack.length,
+        start = Math.max(0, haystack.indexOf('<mark>') - options.start),
+        stop = Math.min(length, options.stop);
+    haystack = haystack.substr(start, stop);
+    haystack = haystack.slice(start ? haystack.indexOf(' ') + 1 : start, stop < length ? haystack.lastIndexOf(' ') : length);
+    haystack = (start ? '... ' : '') + haystack + (stop < length ? ' ...' : '');
+    return haystack;
+  }
+
+  /* SEARCH */
+  (function() {
+    var modal = $('#search-modal'),
+        input = $('#search-modal-input');
+
+    if (!modal.length) return;
+
+    modal.on('shown.bs.modal', function() {
+      $('#search-modal-input').focus();
+    }).on('hidden.bs.modal', function() {
+      $('#search-modal-input').val('');
+      $('#search-modal-results').html('');
     });
-    $('#search-modal-results').html(results.map(function(result) {
-      return '<div>' +
-        '<strong>' +
-          '<a href="' + result.url + '">' + result.title + '</a>' +
-        '</strong>' +
-      '</div>';
-    }).join(''));
+
+    input.on('input', function (evt) {
+      var terms = evt.target.value;
+      $('#search-modal-results').html(terms.length > 3 ? Mustache.render(templates['search'], {
+        results: window.search.map(function (page) {
+          return $.extend({}, page, { content: search(terms, page.content) });
+        }).filter(function (page) {
+          return !!page.content;
+        })
+      }) : '');
+    });
+  }())
+
+  /* MUSTACHE */
+  $('script[type="x-tmpl-mustache"]').each(function (idx, elm) {
+    elm = $(elm);
+    template = elm.attr('data-template');
+    templates[template] = elm.html();
+    Mustache.parse(templates[template]);
+    elm.remove();
   });
-}())
+
+}(jQuery, window, document))
