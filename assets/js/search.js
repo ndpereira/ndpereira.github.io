8421633c4f2a50a6dea71287897e318837a57027
cdeath <github@nunopereira.net>
2019-03-22 14:19:57 +0000

    search

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,12 +1,23 @@
 ---
 layout: null
 ---
+{% assign title = site.settings | where: 'slug', 'general' | map: 'title' | first %}
+{% assign nav = site.navigation | where: 'slug', 'header' | map: 'items' | map: 'title' | push: '' | unshift: title | unshift: title | unshift: '·' %}
 window.search = [
   {% for page in site.pages %}
+  {% assign replace = nav | unshift: page.title | join: ' ' %}
   {
     "title": "{{page.title}}",
     "url": "{{page.url}}",
-    "content": "{{page.output | strip_html | normalize_whitespace }}"
-  }{% unless forloop.last %},{% endunless %}
+    "content": "{{page.output | strip_html | normalize_whitespace | replace: replace, ''}}"
+  },
+  {% endfor %}
+  {% for post in site.posts %}
+  {% assign replace = nav | unshift: post.title | join: ' ' %}
+  {
+    "title": "{{post.title}}",
+    "url": "{{post.url}}",
+    "content": "{{post.output | strip_html | normalize_whitespace | replace: replace, '' }}"
+  }{% unless forloop.last %}, {% endunless %}
   {% endfor %}
 ];
