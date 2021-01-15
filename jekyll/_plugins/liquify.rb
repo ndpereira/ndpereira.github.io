8d5fb546394de3d3ae35643e8baf1710afa6b7d1
cdeath <github@nunopereira.net>
2021-01-15 15:49:52 +0000

    cleanup

--- jekyll/_plugins/liquify.rb
+++
@@ -1,9 +0,0 @@
-module Liquify
-  def liquify(str)
-    str = "#{str}".gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% include picture.html src="\2" alt="\1" \3 %}')
-    str = Liquid::Template.parse(str).render(@context)
-    Kramdown::Document.new(str).to_html
-  end
-end
-
-Liquid::Template.register_filter(Liquify)
